6a617ce9d50622a8d015c601d2a55cab4734271f
ZnakeEYEZ <andreas.malmstrom@hotmail.com>
2015-12-19 12:13:19 +0100

    Update stories.js

--- public/js/stories.js
+++ public/js/stories.js
@@ -1,7 +1,7 @@
 var storyLib = [];
-var imageLib = ['6.jpg', '5.jpg', '4.jpg', '3.png', '2.png', '1.jpg'];
-var titleLib = ['Title6', 'Title5', 'Title4', 'Title3', 'Title2', 'Title1'];
-var descriptionLib = ['description6', 'description5', 'description4', 'description3', 'description2', 'description1'];
+var imageLib = ['6.jpg', '5.jpg', '4.jpg', '3.png', '2.png', '01winter_snow_forest_trees_road.jpg'];
+var titleLib = ['Title6', 'Title5', 'Title4', 'Title3', 'Title2', 'Vinter'];
+var descriptionLib = ['description6', 'description5', 'description4', 'description3', 'description2', 'kvällspromenad'];
 
 var stories =
 {
